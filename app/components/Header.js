51f15b62c4a1c90029f0b503a5091abf368dfda8
Marko Stojanovic <markostojanovic1009@protonmail.com>
2016-09-03 16:36:13 +0200

    Add polls to navigation

--- app/components/Header.js
+++ app/components/Header.js
@@ -18,6 +18,19 @@ class Header extends React.Component {
     const rightNav = this.props.token ? (
       <div className="top-bar-right">
         <ul className="vertical medium-horizontal menu">
+          <li>
+            <ul className="medium-horizontal vertical dropdown menu"
+                data-responsive-menu="accordion medium-dropdown">
+              <li className="has-submenu">
+                <a href="">Polls</a>
+                <ul className="submenu menu vertical nested" data-submenu>
+                  <li><Link to="/polls">All polls</Link></li>
+                  <li><Link to="/new">Create</Link></li>
+                  <li><Link to="#">My polls</Link></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
           <li><Link to="/account" activeClassName="active">My Account</Link></li>
           <li><a href="#" onClick={this.handleLogout.bind(this)}>Logout</a></li>
         </ul>
