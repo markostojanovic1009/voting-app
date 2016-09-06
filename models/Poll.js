5a66f5e091e726c19e930e4ec343be8ebc3e4901
Marko Stojanovic <markostojanovic1009@protonmail.com>
2016-09-06 14:55:13 +0200

    Add comments to Poll model

--- models/Poll.js
+++ models/Poll.js
@@ -31,6 +31,10 @@ const genericMessage = {
  */
 const Poll = {
 
+    /**
+     * Creates a new poll with title and user_id.
+     * Poll options can later be added by calling addPollOptions.
+     */
     createPoll(user_id, title) {
         return new Promise((resolve, reject) => {
             knex('polls').returning(['id', 'user_id', 'title']).insert({user_id, title})
@@ -46,8 +50,11 @@ const Poll = {
         });
     },
 
-    /*
-     * Gets all polls and votes for each poll.
+    /**
+     * Returns polls and aggregated poll results.
+     * Receives either a valid user id, in which case it returns
+     * polls made by the user with that id, or null, in which case
+     * it returns all the polls
      */
     getPolls(user_id) {
         return new Promise((resolve, reject) => {
@@ -58,12 +65,12 @@ const Poll = {
                 .joinRaw('left outer join (SELECT poll_option_id, COUNT(poll_option_id) as vote_count ' +
                     'FROM votes GROUP BY poll_option_id ) as votesCount ' +
                     'ON poll_options.id = votesCount.poll_option_id')
-                .where(user_id ? {user_id} : {})
-                .orderBy('polls.id', 'desc')
+                .where(user_id ? {user_id} : {}) // Filter based on user_id
+                .orderBy('polls.id', 'desc') // Newest polls first
                 .then((result) => {
 
                     /*
-                     * Groups them into an array of poll objects.
+                     * Groups results into an array of poll objects.
                      * Each poll object has a pollId, title, total ( added later )
                      * and options array.
                      * Options is an array of Poll Options Objects, each of which
@@ -99,16 +106,21 @@ const Poll = {
                     // Calculate the total number of votes for each Poll,
                     // and from that a percentage of votes for each Poll Option.
                     groupedResults = groupedResults.map((item) => {
+
                         const total = item.options.reduce((prev, current) => {
                             return prev + current.vote_count
                         }, 0);
+
+                        /*
+                         * Returns 0% nobody voted for the poll.
+                         */
                         return {
                             ...item,
                             total,
                             options: item.options.map((option) => {
                                 return {
                                     ...option,
-                                    percentage: total > 0 ? (option.vote_count / total * 100).toFixed(2) : 'No votes.'};
+                                    percentage: total > 0 ? (option.vote_count / total * 100).toFixed(2) : 0};
                             })
                         };
                     });
@@ -121,11 +133,20 @@ const Poll = {
         });
     },
 
+    /**
+     * Adds additional options to the poll.
+     * Used when creating the poll or when updating it.
+     * pollOptions must be an array of objects with a single property 'text'.
+     */
     addPollOptions(poll_id, pollOptions) {
         return new Promise((resolve, reject) => {
+
             if (!Array.isArray(pollOptions))
                 reject({ msg: 'Second argument must be an array.'});
+
+            // Add a poll_id foreign key to each poll option
             const mappedOptions = pollOptions.map((item) => { return {...item, poll_id }; });
+
             knex('poll_options').insert(mappedOptions).returning(['id', 'poll_id', 'text'])
                 .then((pollOptions) => {
                     resolve(pollOptions);
@@ -139,11 +160,18 @@ const Poll = {
         });
     },
 
+    /**
+     * Inserts a new vote into the vote table.
+     * Checks whether the user voted ( checks for user_id if he is registered and logged in,
+     * otherwise checks for ip address ) to disallow voting multiple times.
+     */
     voteFor(poll_id, poll_option_id, user_id, ip_address) {
         return new Promise((resolve, reject) => {
 
             knex.select('user_id', 'ip_address').from(function() {
+
                 this.select('id as poll_option_id').from('poll_options').where('poll_id', poll_id).as('options');
+
             }).innerJoin('votes', 'votes.poll_option_id', 'options.poll_option_id')
                 .where(function() {
 
@@ -153,11 +181,15 @@ const Poll = {
 
                 })
                 .then((userVotes) => {
+                    // Found either user_id or ip_address that was already
+                    // used to vote in this poll.
                     if (userVotes.length > 0) {
                         throw {
                             type: 'VOTED'
                         }
                     }
+
+                    // Otherwise insert the new vote
                     return knex('votes').insert({poll_option_id, user_id, ip_address});
                 })
                 .then(() => {
@@ -180,38 +212,70 @@ const Poll = {
         });
     },
 
+    /**
+     * Returns poll title, poll options and aggregated poll results.
+     */
     getPollVotes(poll_id) {
         return new Promise((resolve, reject) => {
             let options = [];
             let owner_id = null;
+
+            //Find the owner of the poll first
             knex.select('user_id').from('polls').where('id', poll_id).then(([result]) => {
+
+                // Throw an error if poll_id isn't found
+                if(!result) {
+                    throw {
+                        type: 'INVALID_POLL_ID'
+                    }
+                }
                 owner_id = result.user_id;
+
+                // Find all poll options and their votes
                 return knex.select('options.id as poll_option_id', 'text').count('poll_option_id').from(function () {
                     this.select('id', 'text').from('poll_options').where('poll_id', poll_id).as('options')
                 })
                     .leftOuterJoin('votes', 'votes.poll_option_id', 'options.id')
                     .groupBy('options.id', 'options.text');
             }).then((result) => {
+
+                // Count is returned as a string initially - I guess that's knex behavior -
+                // so I parse them all for future calculations
                 options = result.map((item) => {
                     return {...item, count: parseInt(item.count)};
                 });
+
+                // Finally get poll's title
                 return knex.select('id', 'title').from('polls').where('id', poll_id);
+
             }).then(([poll]) => {
+
                 const total = options.reduce((previous, item) => {
                     return previous + item.count;
                 }, 0);
+
                 resolve([{
                     ...poll,
                     total,
                     options,
                     owner_id
                 }]);
+
             }).catch((error) => {
+
+                if(error.type === 'INVALID_POLL_ID')
+                    reject({
+                        msg: 'Wrong poll id'
+                    });
+
                 reject(genericMessage);
             });
         });
     },
 
+    /**
+     * Delete a poll with a given id
+     */
     deletePoll(poll_id) {
         return new Promise((resolve, reject) => {
             return knex('polls').where('id', poll_id).del().then(() => {
