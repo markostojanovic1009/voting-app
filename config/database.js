a97919feb23dee1c8cb7c01340150d0c7e33cbbe
Marko Stojanovic <markostojanovic1009@protonmail.com>
2016-09-16 19:46:37 +0200

    Add some logging to knex

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 var config = require('../knexfile');
 const environment = process.env.NODE_ENV || 'development';
+console.log('Running on ', environment);
 var knex = require('knex')(config[environment]);
 
 module.exports = knex;
